7baeec8b6b9909e24862cdf750aeaf2e65c6aa1c
edward <edysfy@icloud.com>
2021-03-10 14:35:53 +0000

    added server stuff

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/demosite/index.html'));
 });
 
 /**
