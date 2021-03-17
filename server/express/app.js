72ecafd6e9547a344e250bb552b0d5d246b1c7d8
edward <edysfy@icloud.com>
2021-03-17 13:40:22 +0000

    readding ability to use node server.js to localhost 3000

--- server/express/app.js
+++ server/express/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 const mongoDBConnect = "mongodb+srv://Hari:G8F1P3MeLb77pV48@cluster0.9hjpe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
@@ -17,6 +18,12 @@ app.use(bodyParser.json());
 /*parses urlencoded bodies*/
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use(express.static(path.join(__dirname, '../../dist/demosite')));
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/demosite/index.html'));
+});
+
 /*connect to mongoDb*/
 mongoose.connect(mongoDBConnect,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(connection => {
@@ -25,6 +32,7 @@ mongoose.connect(mongoDBConnect,{ useNewUrlParser: true, useUnifiedTopology: tru
 .catch(err=>{
     console.log("Failed to connect ot mongoDb");
 })
+
 
 
 app.use("/user",userRoutes);
