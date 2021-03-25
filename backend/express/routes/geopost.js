08bb6bab70002e4b5919a0fed2df2ce82a23ec33
Hari <haripatel130@gmail.com>
2021-03-25 11:14:52 +0000

    n

--- backend/express/routes/geopost.js
+++ backend/express/routes/geopost.js
@@ -2,40 +2,7 @@ const express = require("express");
 const router = express.Router();
 const GeoJson = require("../../mongo_schema/geoJson");
 
-/*saves a post to the database*/
-router.post("", (req, res, next) => {
-  const newPost = new GeoJson({
-    type: req.body.type,
-    location: {
-      type: req.body.location.type,
-      coordinates: [
-        req.body.location.coordinates[0],
-        req.body.location.coordinates[1],
-      ],
-    },
-    properties: {
-      username: req.body.properties.username,
-      dateTime: req.body.properties.dateTime,
-      keyword: req.body.properties.keyword,
-      mood: req.body.properties.mood,
-      textBody: req.body.properties.textBody,
-    },
-  });
-  newPost
-    .save()
-    .then((dbResponse) => {
-      return res.status(200).json({
-        message: "geoPost saved in database",
-        id: dbResponse,
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        message: "unable to save this data",
-        error: error,
-      });
-    });
-});
+
 
 /*gets all post from the db*/
 router.get("", (req, res, next) => {
@@ -56,25 +23,3 @@ router.get("", (req, res, next) => {
   });
 });
 
-/*removes post from the db*/
-router.delete("", (req, res, next) => {
-  GeoJson.deleteOne({ _id: req.body._id })
-    .then((result) => {
-      res
-      .status(200)
-      .json({
-        message: "Post deleted",
-        result: result,
-      });
-    })
-    .catch((error) => {
-      res
-      .status(401)
-      .json({
-        message: "Post doesn't exist",
-        error: error,
-      });
-    });
-});
-
-module.exports = router;
