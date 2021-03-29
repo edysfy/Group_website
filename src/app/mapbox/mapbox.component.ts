e36406d3d7af7e7416643328a239c0b5bcb08ef8
Tarn Williamson <tarnwilliamson123@googlemail.com>
2021-03-29 17:23:51 +0100

    added geolocation

--- src/app/mapbox/mapbox.component.ts
+++ src/app/mapbox/mapbox.component.ts
@@ -17,22 +17,40 @@ export class MapboxComponent implements OnInit {
 
   ngOnInit(): void {
     (mapboxgl as any).accessToken = environment.mapboxToken;
-    this.map = new mapboxgl.Map({
+    var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     zoom: 12,
     center: [-122.447303, 37.753574]
     });
-    this.map.on("click", function(e:any){
-      console.log("background click", e.lngLat);
-      var geojson = {
-          type: "FeatureCollection",
-          features: [{
-              type:"Feature",
-              geometry: { type: "Point", coordinates: [ e.lngLat.lng, e.lngLat.lat ]}
-          }]
-      };
-  });
-  }
 
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true
+      })
+      );
+
+    map.on('click', function(e:any) {
+      var popup = new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML("<center>Coordinates collected. Redirecting to form...</center>")
+        .addTo(map);
+    });
+
+
+
+  //   this.map.on("click", function(e:any){
+  //     console.log("background click" + e.lngLat);
+  //     var geojson = {
+  //         type: "FeatureCollection",
+  //         features: [{
+  //             type:"Feature",
+  //             geometry: { type: "Point", coordinates: [ e.lngLat.lng, e.lngLat.lat ]}
+  //         }]
+  //     };
+
+  }
 }
