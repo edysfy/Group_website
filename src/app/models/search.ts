a1289619b5360f55a336b3e82fd1720e50db3b54
Hari <haripatel130@gmail.com>
2021-04-18 08:01:00 +0100

    implemented two way data binding on the search elements. I didnt create a form. I used ngmodel to bind the components to varables in the class, also the init values in the class show in the ui due to 2 way data binding,when there is a change in a component i create a new search object withe valkues in the class and update the state with the new search object

---
+++ src/app/models/search.ts
@@ -0,0 +1,11 @@
+export interface Search {
+    minAge: number,
+    maxAge: number,
+    minDay: number,
+    maxDay: number,
+    copingWell: boolean,
+    depression: boolean,
+    anxiety: boolean,
+    male: boolean,
+    female: boolean,
+}
--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -2,14 +2,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
 import { UserSearchService } from '../service/user-search.service';
 import { MatCheckboxChange } from '@angular/material/checkbox';
-
+import { Search } from '../models/search';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
-  styleUrls: ['./user-search.component.css']
+  styleUrls: ['./user-search.component.css'],
 })
-export class UserSearchComponent implements OnInit,OnDestroy {
+export class UserSearchComponent implements OnInit, OnDestroy {
   ageMin: number = 0;
   ageMax: number = 100;
   optionAge: Options = {
@@ -23,30 +24,56 @@ export class UserSearchComponent implements OnInit,OnDestroy {
     floor: -3650,
     ceil: 0,
     step: 1,
-  }
+  };
   mood: any = {
-    coping: true,
-    depressed: true,
+    copingWell: true,
+    depression: true,
     anxiety: true,
-  }
+  };
   gender: any = {
     male: true,
     female: true,
-  }
+  };
+  searchQueryState!: BehaviorSubject<Search>;
 
-  constructor(private userSearchService: UserSearchService) { }
+  constructor(private userSearchService: UserSearchService) {
+    this.searchQueryState = new BehaviorSubject<Search>({
+      minAge: this.ageMin,
+      maxAge: this.ageMax,
+      minDay: this.daysMin,
+      maxDay: this.daysMax,
+      copingWell: this.mood.copingWell,
+      depression: this.mood.depression,
+      anxiety: this.mood.anxiety,
+      male: this.gender.male,
+      female: this.gender.male,
+    });
+  }
 
   ngOnInit(): void {
     this.userSearchService.setHasSearchInit(true);
-
+    this.searchQueryState.subscribe(state => {
+      console.log(state);
+    })
   }
 
   ngOnDestroy(): void {
     this.userSearchService.setHasSearchInit(false);
   }
 
-  moodChange(){
-    console.log(this.mood);
+  onChange() {
+    let search = {
+      minAge: this.ageMin,
+      maxAge: this.ageMax,
+      minDay: this.daysMin,
+      maxDay: this.daysMax,
+      copingWell: this.mood.copingWell,
+      depression: this.mood.depression,
+      anxiety: this.mood.anxiety,
+      male: this.gender.male,
+      female: this.gender.male,
+    }
+    this.searchQueryState.next(search);
   }
- 
+
 }
